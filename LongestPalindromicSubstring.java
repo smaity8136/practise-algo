9066d4c792e1f57b0dc2819b9db6f104d8bed313
Subhadip <11837840+smaity8136@users.noreply.github.com>
2020-06-18 17:35:47 -0500

    Create LongestPalindromicSubstring.java

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,49 @@
+package com.practice.test;
+/**
+ *
+ * Example 1:
+ *
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: "cbbd"
+ * Output: "bb"
+ * */
+public class LongestPalindromicSubstring {
+
+    private static int lo, maxLen;
+
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len-1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i+1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private static void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        String str ="babad";
+
+        String rs = longestPalindrome(str);
+
+        System.out.println("longest palindrom "+rs);
+    }
+
+}
